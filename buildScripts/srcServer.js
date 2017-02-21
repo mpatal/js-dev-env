6b0064c0866bfacc2487d0c8539a8c2d982582f2
Marvin <marvin.patal@hotmail.com>
2017-02-20 20:11:14 -0500

    Add json server mock support

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -26,3 +26,11 @@ app.listen(port, function(err){
     open('http://localhost:' + port);
   }
 });
+
+app.get('/users', function(req, res) {
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName":"Smith", "email":"bob@gmail.com"},
+    {"id": 2, "firstName": "Tammy", "lastName":"Norton", "email":"tnorton@gmail.com"},
+    {"id": 3, "firstName": "Tina", "lastName":"Lee", "email":"tleen@gmail.com"}
+  ]);
+});
